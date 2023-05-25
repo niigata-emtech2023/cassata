b87e4a935cf323b026d2bc098b68920e2ea11082
emtech-user <emtech-user@192.168.128.106>
2023-05-25 15:24:42 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/niigata-emtech2023/cassata

--- SENGOKU_PORTAL/src/main/java/model/entity/BushoBean.java
+++ SENGOKU_PORTAL/src/main/java/model/entity/BushoBean.java
@@ -1,7 +1,112 @@
 package model.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class BushoBean implements Serializable{
+	
+	//武将ID
+	private String busho_id;
+	
+	//武将名
+	private String busho_name;
+	
+	//時代ID
+	private String period_id;
+	
+	//説明文
+	private String commentary;
+	
+	//生年月日
+	private Date birth_date;
+	
+	//武将画像
+	private String busho_img;
+
+	/**
+	 * @return busho_id
+	 */
+	public String getBushoID() {
+		return busho_id;
+	}
+
+	/**
+	 * @param busho_id セットする busho_id
+	 */
+	public void setBushoID(String busho_id) {
+		this.busho_id = busho_id;
+	}
+
+	/**
+	 * @return busho_name
+	 */
+	public String getBushoName() {
+		return busho_name;
+	}
+
+	/**
+	 * @param busho_name セットする busho_name
+	 */
+	public void setBushoName(String busho_name) {
+		this.busho_name = busho_name;
+	}
+
+	/**
+	 * @return period_id
+	 */
+	public String getPeriodID() {
+		return period_id;
+	}
+
+	/**
+	 * @param period_id セットする period_id
+	 */
+	public void setPeriodID(String period_id) {
+		this.period_id = period_id;
+	}
+
+	/**
+	 * @return commentary
+	 */
+	public String getCommentary() {
+		return commentary;
+	}
+
+	/**
+	 * @param commentary セットする commentary
+	 */
+	public void setCommentary(String commentary) {
+		this.commentary = commentary;
+	}
+
+	/**
+	 * @return birth_date
+	 */
+	public Date getBirth_date() {
+		return birth_date;
+	}
+
+	/**
+	 * @param birth_date セットする birth_date
+	 */
+	public void setBirth_date(Date birth_date) {
+		this.birth_date = birth_date;
+	}
+
+	/**
+	 * @return busho_img
+	 */
+	public String getBusho_img() {
+		return busho_img;
+	}
+
+	/**
+	 * @param busho_img セットする busho_img
+	 */
+	public void setBusho_img(String busho_img) {
+		this.busho_img = busho_img;
+	}
+	
+	
 	
 }
