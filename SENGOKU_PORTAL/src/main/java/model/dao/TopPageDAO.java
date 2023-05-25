f36ca223b13c936f16df218ed705964eeae6b5f6
nmn29 <ina010218@gmail.com>
2023-05-25 15:41:55 +0900

    add userbean, rankingselect

--- SENGOKU_PORTAL/src/main/java/model/dao/TopPageDAO.java
+++ SENGOKU_PORTAL/src/main/java/model/dao/TopPageDAO.java
@@ -1,6 +1,11 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.entity.UserBean;
@@ -9,8 +14,30 @@ public class TopPageDAO{
 	
 	public List<UserBean> selectRanking() throws SQLException, ClassNotFoundException{
 		
+		List<UserBean> rankingList = new ArrayList<UserBean>();
 		
-		return null;
+		String sql = "SELECT b.busho_name, COUNT(*) AS count FROM user u INNER JOIN busho b ON (u.busho_id = b.busho_id) GROUP BY b.busho_name ORDER BY COUNT(*) DESC";
+
+		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
+		try (Connection con = ConnectionManager.getConnection();
+				Statement stmt = con.createStatement();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+			
+			ResultSet res = pstmt.executeQuery();
+
+			// 結果の操作
+			while (res.next()) {
+				String busho_name = res.getString("busho_name");
+				int count = res.getInt("count");
+
+				UserBean user = new UserBean();
+				user.setBushoName(busho_name);
+				user.setCount(count);
+
+				rankingList.add(user);
+			}
+		}
+		return rankingList;
 		
 	}
 }
