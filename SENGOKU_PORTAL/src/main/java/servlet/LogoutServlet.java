d3729f8792940b7c76bd62ed7cc8c13a89689901
emtech-user <emtech-user@192.168.128.113>
2023-05-25 16:52:33 +0900

    aaa

--- SENGOKU_PORTAL/src/main/java/servlet/LogoutServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/LogoutServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutServlet
@@ -34,8 +37,16 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		// セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+
+		// セッションオブジェクトの無効化
+		session.invalidate();
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("start.html");
+		rd.forward(request, response);
 	}
 
 }
--- SENGOKU_PORTAL/src/main/java/servlet/RegisterServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/RegisterServlet.java
@@ -1,11 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class RegisterServlet
@@ -34,8 +38,46 @@ public class RegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		
+		// リクエストオブジェクトのエンコーディング方式の指定
+				request.setCharacterEncoding("UTF-8");
 
-}
+				// リクエストパラメータの取得
+				String user_id = request.getParameter("user_id");
+				String password = request.getParameter("password");
+				String nickname = request.getParameter("nickname");
+
+				request.setCharacterEncoding("UTF-8");
+
+				// セッションオブジェクトの取得
+				HttpSession session = request.getSession();
+
+				// セッションスコープへの属性の設定
+				session.setAttribute("user_id", user_id);
+				session.setAttribute("password", password);
+				session.setAttribute("nickname", nickname);
+
+				// DAOの生成
+				RegisterDAO register = new RegisterDAO();
+
+				int count = 0;	// 処理件数
+
+				try {
+					// DAOの利用
+					count = register.insert(employee);
+
+				} catch (ClassNotFoundException | SQLException e) {
+					e.printStackTrace();
+				}
+
+				// リクエストスコープへの属性の設定
+				request.setAttribute("count", count);
+				request.setAttribute("employee", employee);
+
+				// リクエストの転送
+				RequestDispatcher rd = request.getRequestDispatcher("employee-registration-result.jsp");
+				rd.forward(request, response);
+
+			}
+
+		}
