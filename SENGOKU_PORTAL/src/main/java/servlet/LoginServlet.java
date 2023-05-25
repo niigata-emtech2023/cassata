c388b56af5a08f3f5434adb8005133c37371251d
emtech-user <emtech-user@192.168.128.113>
2023-05-25 16:57:24 +0900

    Merge branch 'master' of https://github.com/niigata-emtech2023/cassata

--- SENGOKU_PORTAL/src/main/java/servlet/LoginServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/LoginServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.dao.LoginDAO;
+
 /**
  * Servlet implementation class LoginServlet
  */
