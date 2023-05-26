bbffc8e1dd300dfffa31b8f1f73b0d8bf4997663
emtech-user <emtech-user@192.168.128.113>
2023-05-26 15:28:36 +0900

    servlet

--- SENGOKU_PORTAL/src/main/java/servlet/ChangeBushoServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/ChangeBushoServlet.java
@@ -1,11 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.BushoDAO;
 
 /**
  * Servlet implementation class ChangeBushoServlet
@@ -34,8 +42,52 @@ public class ChangeBushoServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		
+		// リクエストオブジェクトのエンコーディング方式の指定
+		request.setCharacterEncoding("UTF-8");
+		// セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+
+		// リクエストパラメータの取得
+		String busho_id = request.getParameter("busho_id");
+		String busho_name = request.getParameter("busho_name");
+		String period_id = request.getParameter("period_id");
+		String commentary = request.getParameter("commentary");
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+		Date birth_date = sdFormat.parse(birth_date);
+		String busho_img = request.getParameter("busho_img");
+
+		// リクエストスコープへの属性の設定
+		request.setAttribute("busho_id", busho_id);
+		request.setAttribute("busho_name", busho_id);
+		request.setAttribute("period_id", period_id);
+		request.setAttribute("commentary", commentary);
+		request.setAttribute("birth_date", birth_date);
+		request.setAttribute("busho_img", busho_img);
+		
+
+		// DAOの生成
+		BushoDAO bushodao = new BushoDAO();
+		
+		int count = 0;	// 処理件数
+
+		try {
+			// DAOの利用
+			count = bushodao.changeBusho (busho_id, busho_name, period_id, commentary, birth_date, busho_img);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		// リクエストスコープへの属性の設定
+		request.setAttribute("count", count);
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+		rd.forward(request, response);
+
+
 	}
 
 }
--- SENGOKU_PORTAL/src/main/java/servlet/DeleteBushoServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/DeleteBushoServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.BushoDAO;
 
 /**
  * Servlet implementation class DeleteBushoServlet
@@ -26,16 +32,47 @@ public class DeleteBushoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
+		// リクエストオブジェクトのエンコーディング方式の指定
+		request.setCharacterEncoding("UTF-8");
+		// セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+
+		// リクエストパラメータの取得
+		String busho_id = request.getParameter("busho_id");
+
+		// リクエストスコープへの属性の設定
+		request.setAttribute("busho_id", busho_id);
+
+		// DAOの生成
+		BushoDAO bushodao = new BushoDAO();
+		
+		int count = 0;	// 処理件数
+
+		try {
+			// DAOの利用
+			count = bushodao.deleteBusho (busho_id);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		// リクエストスコープへの属性の設定
+		request.setAttribute("count", count);
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+		rd.forward(request, response);
+
+
 	}
 
 }
