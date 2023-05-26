1876715449a52253a44c8a0d48f333669a30d1ad
emtech-user <emtech-user@192.168.128.113>
2023-05-26 15:01:36 +0900

    servlet

--- SENGOKU_PORTAL/src/main/java/servlet/DeleteCustomerServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/DeleteCustomerServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.UserDAO;
 
 /**
  * Servlet implementation class DeleteCustomerServlet
@@ -34,8 +40,43 @@ public class DeleteCustomerServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		
+		// リクエストオブジェクトのエンコーディング方式の指定
+				request.setCharacterEncoding("UTF-8");
+				
+				// セッションオブジェクトの取得
+				HttpSession session = request.getSession();
 
-}
+				// リクエストパラメータの取得
+				String user_id = request.getParameter("user_id");
+				String password = request.getParameter("password");
+				String nickname = request.getParameter("nickname");
+
+				// セッションスコープへの属性の設定
+				request.setAttribute("user_id", user_id);
+				request.setAttribute("password", password);
+				request.setAttribute("nickname", nickname);
+
+				// DAOの生成
+				UserDAO registerdao = new UserDAO();
+
+				int count = 0;	// 処理件数
+
+				try {
+					// DAOの利用
+					count = registerdao.deleteProfile(user_id);
+
+				} catch (ClassNotFoundException | SQLException e) {
+					e.printStackTrace();
+				}
+
+				// リクエストスコープへの属性の設定
+				request.setAttribute("count", count);
+
+				// リクエストの転送
+				RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+				rd.forward(request, response);
+
+			}
+
+		}
