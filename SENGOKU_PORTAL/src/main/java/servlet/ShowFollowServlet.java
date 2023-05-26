3e645099bbf2378946a07a04758619060494e88f
emtech-user <emtech-user@192.168.128.113>
2023-05-26 11:17:21 +0900

    servlet

--- SENGOKU_PORTAL/src/main/java/servlet/ShowFollowServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/ShowFollowServlet.java
@@ -1,11 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.FollowDAO;
+import model.entity.FollowBean;
 
 /**
  * Servlet implementation class ShowFollowServlet
@@ -34,8 +42,42 @@ public class ShowFollowServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		// リクエストオブジェクトのエンコーディング方式の指定
+		request.setCharacterEncoding("UTF-8");
+
+		// リクエストパラメータの取得
+		String user_id = request.getParameter("user_id");
+		String follow_user_id = request.getParameter("follow_user_id");
+
+		request.setCharacterEncoding("UTF-8");
+
+		// セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+
+		// セッションスコープへの属性の設定
+		session.setAttribute("user_id", user_id);
+		session.setAttribute("follow_user_id", follow_user_id);
+
+		List<FollowBean> followList = null;
+
+		// DAOの生成
+		FollowDAO followdao = new FollowDAO();
+
+		try {
+			// DAOの利用
+			followList = followdao.selectFollow();
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// セッションスコープへの属性の設定
+		session.setAttribute("followList", followList);
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("follow.jsp");
+		rd.forward(request, response);
+		
 	}
 
 }
