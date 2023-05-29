ac2d79c574e8bbf3faa11e7b386a2a11e2f0fa31
emtech-user <emtech-user@192.168.20.18>
2023-05-29 14:13:03 +0900

    Merge branch 'master' of https://github.com/niigata-emtech2023/cassata

---
+++ SENGOKU_PORTAL/src/main/java/servlet/BushoSendServlet.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BushoSendServlet
+ */
+@WebServlet("/BushoSendServlet")
+public class BushoSendServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BushoSendServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+
+		// リクエストパラメータの取得
+		String busho_img = request.getParameter("busho_img");
+		String busho_name = request.getParameter("busho_name");
+		String period_id = request.getParameter("period_id");
+		String birth_date = request.getParameter("birth_date");
+		String commentary = request.getParameter("commentary");	
+		
+		request.setAttribute("busho_name",busho_name );
+		request.setAttribute("period_id",period_id );
+		request.setAttribute("birth_date",birth_date);
+		request.setAttribute("commentary",commentary);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("changeBushoListConfilm");
+		rd.forward(request, response);
+	}
+
+}
--- SENGOKU_PORTAL/src/main/webapp/changeBushoList.jsp
+++ SENGOKU_PORTAL/src/main/webapp/changeBushoList.jsp
@@ -11,30 +11,31 @@
 		List<BushoBean> bushoList = (List<BushoBean>) request.getAttribute("bushoList");
 	%>
 	
-    <form action="changeBushoConfilm.jsp" method="POST">
+    <form action="BushoSendServlet" method="POST">
     
-    <% 
-    	for(BushoBean busho : bushoList){ 
-    %>
-    <!--顔写真：--><img src="<%=busho.getBushoImg()%>" alt="武将の顔写真"><br>
-            <input type="" name="busho_img">
+    <%
+		BushoBean employee = (BushoBean) request.getAttribute("busho");
+	%>
 	
+	<% for(BushoBean busho : bushoList){ %>
+    <!--顔写真：-->
+    <select name="">
+    	<option value="<%=busho.getBushoImg()%>"></option>
+	</select>
     名前：
     <input type="text" name="busho_name" value="<%=busho.getBushoName()%>"><br>
     
     年代：
-    <input type="text" name="period_id" value="<%=busho.getPeriodName()%>"><br>
-    
+    <input type="text" name="period_id" value="<%=busho.getPeriodID()%>"><br>
     
     生年月日：
     <input type="text" name="birth_date" value="<%=busho.getBirthDate()%>"><br>
               
     説明文：
     <input type="text" name="commentary" <%=busho.getCommentary()%>><br>
-    <% } %>
     
     <input type="submit" value="変更する">
+    <% } %>
     </form>
-    .fewwqfewq qfewpjfｊふぇをいｆｑっふぇｗくぉ>e<><% aaaa%>/%>
 </body>
 </html>
