2344c65147d5e441b54f3a424d5a1da54059855c
emtech-user <emtech-user@192.168.128.105>
2023-05-29 16:27:15 +0900

    jsp

--- SENGOKU_PORTAL/src/main/java/servlet/ProfileChangeSendServlet.java
+++ SENGOKU_PORTAL/src/main/java/servlet/ProfileChangeSendServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RegisterSendServlet
  */
+//changeProfile.jspから転送
 @WebServlet("/ProfileChangeSendServlet")
 public class ProfileChangeSendServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
